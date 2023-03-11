f0a3603021d685aba08819f689b5d5e06e3e3131
Nirav kumbhani <109146949+NiravKumbhani30@users.noreply.github.com>
2023-03-11 09:13:18 +0530

    url change

--- helper/baseURL.js
+++ helper/baseURL.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://nick-crud.vercel.app/"
+    ? "https://crud-livid-beta.vercel.app/"
     : "http://localhost:3000";
 
 export default BASE_URL;

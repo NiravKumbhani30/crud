ff1da446b909e2388e4f6a55e4325de295ca5f0b
Nirav kumbhani <109146949+NiravKumbhani30@users.noreply.github.com>
2023-03-11 09:17:07 +0530

    d

--- helper/baseURL.js
+++ helper/baseURL.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://crud-livid-beta.vercel.app/"
+    ? "https://crud-livid-beta.vercel.app"
     : "http://localhost:3000";
 
 export default BASE_URL;
